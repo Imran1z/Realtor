fd91773671694c9279600864c461cb1584cddd50
Imran1z <shaikhzimran12334@gmail.com>
2024-02-17 17:40:13 +0530

    user update api completed and made the verifyToken middleware

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { google, signin,signout,signup } from '../controllers/auth.controller.js';
+import { google, signin,signOut,signup } from '../controllers/auth.controller.js';
 
 
 const router =express.Router();
@@ -7,6 +7,6 @@ const router =express.Router();
 router.post('/signup',signup)
 router.post('/signin',signin)
 router.post('/google',google)
-router.get('/signout',google)
+router.get('/signout',signOut)
 
 export default router
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,9 @@
 import express from 'express'
+import { updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router =express.Router();
 
-router.get('/get',(req, res)=>{
-    res.json("lolu")
-})
+router.post('/update/:id',verifyToken,updateUser)
 
 export default router
