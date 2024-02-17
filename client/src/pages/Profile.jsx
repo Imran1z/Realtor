4a2c31444bbf65dc22e7616ae4ed1d905a61e4ca
Imran1z <shaikhzimran12334@gmail.com>
2024-02-17 17:11:06 +0530

    file upload to firebase and profile ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,158 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { Link } from 'react-router-dom';
+import {app} from '../firebase.js'
 
 const Profile = () => {
+  const {currentUser,loading,error}= useSelector((state)=>state.user)
+  const [formData, setFormData] = useState({});
+  const [file,setFile]=useState(undefined)
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const fileRef =useRef(null);
+
+
+  useEffect(()=>{
+    if (file) {
+      handleFileUpload(file);
+    }
+  },[file])
+
+
+
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        console.log(error)
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
+
+
+
+
+  const handleChange =(e)=>{
+    setFormData({...formData , [e.target.id]:e.target.value})
+  }
+
+
+
+  const handleImageClick = () => {
+    fileRef.current.click(); // Trigger the file input dialog
+  };
+
+
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    console.log(formData)
+  }
+
+
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+        <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          <input type="file" onChange={(e)=>setFile(e.target.files[0])} hidden accept='image/*' ref={fileRef}/>
+
+          <img src={formData.avatar || currentUser.avatar} alt="profile"  onClick={handleImageClick} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+
+          <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image successfully uploaded!</span>
+          ) : (
+            ''
+          )}
+        </p>
+
+
+          <input
+          type='text'
+          placeholder='username'
+          defaultValue={currentUser.username}
+          id='username'
+          className='border p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='email'
+          id='email'
+          defaultValue={currentUser.email}
+          className='border p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          onChange={handleChange}
+          id='password'
+          className='border p-3 rounded-lg'
+        />
+
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Update'}
+        </button>
+
+
+        <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+         // to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
+
+        <div className='flex justify-between mt-5'>
+        <span
+          className='text-red-700 cursor-pointer'
+        >
+          Delete account
+        </span>
+        <span  className='text-red-700 cursor-pointer'>
+          Sign out
+        </span>
+        </div>
+
+
+        </form>
+    
+    </div>
   )
 }
 
