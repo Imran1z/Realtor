886036c7e2373894b515d7cc77caf91f8ccf37c0
Imran1z <shaikhzimran12334@gmail.com>
2024-02-17 11:05:48 +0530

    signup and signin ui and functionality is completed

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -5,5 +5,6 @@ import { signin,signout,signup } from '../controllers/auth.controller.js';
 const router =express.Router();
 
 router.post('/signup',signup)
+router.post('/signin',signin)
 
 export default router
