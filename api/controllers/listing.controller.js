6e66e995ac459f6c05aa4a6df609769296e4d910
Imran1z <shaikhzimran12334@gmail.com>
2024-02-23 16:08:42 +0530

    delete listing and resolved a typo

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -17,17 +17,25 @@ export const createListing =async(req, res, next)=>{
 
 
 
+export const deleteListing = async(req, res,next)=>{
 
+    const listing =await Listing.findById(req.params.id);
 
+    if (!listing) {
+        return next(errorHandler(404, "Listing not found"))
+        
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(404, "You can delete only your listings"))
+        
+    }
+    try {
 
-
-
-
-
-
-
-
-export const getListing =(req, res)=>{
-    console.log("done and dusted")
+        await Listing.findByIdAndUpdate(req.params.id);
+        res.status(200).json('Listing has been deleted');
+        
+    } catch (error) {
+        next(error)
+    }
     
 }
