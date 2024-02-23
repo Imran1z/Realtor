31166b3c2fdc0ba7365209783fc500e647d2dde1
Imran1z <shaikhzimran12334@gmail.com>
2024-02-23 16:40:26 +0530

    edit listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -39,3 +39,24 @@ export const deleteListing = async(req, res,next)=>{
     }
     
 }
+
+
+export const updateListing =async(req, res, next)=>{
+    const listing =await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, "Listing not found"))
+        
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(404, "You can Edit only your listings"))
+        
+    }
+    try {
+        const updatedlisting =await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true})
+        res.status(200).json(updatedlisting);
+
+    } catch (error) {
+        next(error)
+    }
+}
