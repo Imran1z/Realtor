398109d7ceeaa280a4286d8eab62e4bc67f3a738
Imran1z <shaikhzimran12334@gmail.com>
2024-02-27 00:02:14 +0530

    9:23

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,41 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = () => {
+    const [sidebarData, setsidebarData] = useState({
+        searchTerm:'',
+        type:'all',
+        parking:false,
+        furnished:false,
+        offer:false,
+        sort:'createdAt',
+        order:'desc',
+    });
+    console.log(sidebarData)
+
+    const handleChange =(e)=>{
+
+        if (e.target.id==='all'||e.target.id==='rent'||e.target.id==='sell') {
+            setsidebarData({...sidebarData,type:e.target.id})
+        }
+        if (e.target.id==='searchTerm') {
+            setsidebarData({...sidebarData,searchTerm:e.target.value})
+            
+        }
+        if (e.target.id==='offer'||e.target.id==='furnished'||e.target.id==='parking') {
+            setsidebarData({...sidebarData,[e.target.id]:e.target.checked||e.target.checked==='true'?true:false})
+        }
+        if(e.target.id==='sort_order'){
+            const sort =e.target.value.split('_')[0]||'createdAt';
+            const order =e.target.value.split('_')[1]||'desc';
+            setsidebarData({...sidebarData,sort,order})
+        }
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+
+        
+    }
   return (
     <div className='flex flex-col md:flex-row'>
         <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
-            <form className='flex flex-col gap-8'>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-8'>
                 <div className='flex items-center gap-2  '>
                     <label className='whitespace-nowrap font-semibold' >Search Term</label>
-                    <input type="text" id='searchTerm' placeholder='search...' className='border rounded-lg p-3 w-full'/>
+                    <input type="text" id='searchTerm' placeholder='search...' className='border rounded-lg p-3 w-full' value={sidebarData.searchTerm} onChange={handleChange}/>
                 </div>
                 <div className='items-center flex gap-2 md:flex-wrap'>
                     <label className='font-semibold' >Type:</label>
                     <div className='flex gap-2 '>
-                        <input type="checkbox" id='all' className='w-5'/>
+                        <input type="checkbox" id='all' className='w-5'
+                            onChange={handleChange}
+                            checked={sidebarData.type==='all'}
+                        />
                         <span>Rent & Sale</span>
                     </div>
                     <div className='flex gap-2 '>
-                        <input type="checkbox" id='rent' className='w-5'/>
+                        <input type="checkbox" id='rent' className='w-5'
+                            onChange={handleChange}
+                            checked={sidebarData.type==='rent'}
+                        />
                         <span>Rent</span>
                     </div>
                     <div className='flex gap-2 '>
-                        <input type="checkbox" id='sell' className='w-5'/>
+                        <input type="checkbox" id='sell' className='w-5'
+                             onChange={handleChange}
+                            checked={sidebarData.type==='sell'}
+                        />
                         <span>Sale</span>
                     </div>
                     <div className='flex gap-2 '>
-                        <input type="checkbox" id='offer' className='w-5'/>
+                        <input type="checkbox" id='offer' className='w-5'
+                             onChange={handleChange}
+                            checked={sidebarData.offer}
+                        />
                         <span>offer</span>
                     </div>
                 </div>
                 <div className='items-center flex gap-2 md:flex-wrap'>
                     <label className='font-semibold'>Amenities:</label>
                     <div className='flex gap-2 '>
-                        <input type="checkbox" id='parking' className='w-5'/>
+                        <input type="checkbox" id='parking' className='w-5'
+                             onChange={handleChange}
+                            checked={sidebarData.parking}
+                        />
                         <span>Parking</span>
                     </div>
                     <div className='flex gap-2 '>
-                        <input type="checkbox" id='furnished' className='w-5'/>
+                        <input type="checkbox" id='furnished' className='w-5'
+                             onChange={handleChange}
+                            checked={sidebarData.furnished}
+                        />
                         <span>Furnished</span>
                     </div>
                     
@@ -43,11 +96,12 @@ const Search = () => {
                 <div className='items-center flex gap-2 '>
                     <label className='font-semibold'>Sort:</label>
                     <div className='flex gap-2 '>
-                        <select name="" id="sort_order" className='border rounded-lg p-3 '>
-                            <option >Price high to low</option>
-                            <option >Price low to high</option>
-                            <option >Latest</option>
-                            <option >Oldest</option>
+                        <select onChange={handleChange}
+                        defaultValue={'createdAtDesc'} name="" id="sort_order" className='border rounded-lg p-3 '>
+                            <option value={'regularPrice_desc'}>Price high to low</option>
+                            <option value={'regularPrice_asc'} >Price low to high</option>
+                            <option value={'createdAt_desc'} >Latest</option>
+                            <option value={'createdAt_asc'}>Oldest</option>
                         </select>
                     </div>
                    
