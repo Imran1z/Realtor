fcbdc67f66d0f443edb90974c9f8bb2788df5510
Imran1z <shaikhzimran12334@gmail.com>
2024-03-20 09:54:16 +0530

    Revert "doplyment pre insatallation"
    This reverts commit 13ace5f65674dad26ff5b210091537bb5d2026db.

--- api/index.js
+++ api/index.js
@@ -5,25 +5,6 @@ import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.router.js'
-
-
-//changes
-import cors from 'cors'
-// Allow requests from a specific origin
-const corsOptions = {
-  origin: 'https://househaven-k1n3.onrender.com/',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
-
-
-import url from 'url';
-import path from 'path'
-
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 dotenv.config();
 
 const app= express();
@@ -40,16 +21,6 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 }).catch((err)=>{
     console.log(err)
 })
-
-// app.use(express.static(path.join(__dirname,'/client/dist')));
-// app.get('*',(req,res)=>{
-//     res.sendFile(path.join(__dirname,'client','dist','index.html'))
-// })
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
 
 app.use('/api/v1/user',userRouter)
 app.use('/api/v1/auth',authRouter)
